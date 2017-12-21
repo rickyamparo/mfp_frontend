f8d4f2703b49db47ff5a933e351991c212f4f9ae
Ricky Amparo <rickyamparo@gmail.com>
2017-12-20 20:11:21 -0700

    standardize file naming conventions, move splash page to proper folder and remove boiler plate fluff from App.js

--- App.js
+++ App.js
@@ -1,30 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import Splash from './Splash'
-import Login from './src/components/Login/Login'
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Splash from './src/components/splash/splash'
+import Login from './src/components/login/login'
+import Register from './src/components/register/register'
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <Login />
+      <Splash />
     );
   }
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
-import LoginForm from './LoginForm'
+import LoginForm from './login_form'
 
 export default class Login extends Component {
   render() {
