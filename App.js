4846adabc5677b67c83111bc61e0b7ff02f7d528
Ricky Amparo <rickyamparo@gmail.com>
2017-12-21 19:43:01 -0700

    implement navigation in App.js

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation'
 import Splash from './src/components/splash/splash'
 import Login from './src/components/login/login'
 import Register from './src/components/register/register'
 
+class LoginScreen extends Component {
+  static navigationOptions = {
+    title: 'Welcome'
+  }
+  render() {
+    return <Login />
+  }
+}
+
+export const SimpleApp = StackNavigator({
+  Login: {screen: LoginScreen}
+})
+
 export default class App extends Component<{}> {
   render() {
     return (
-      <Login />
+      <SimpleApp />
     );
   }
 }
