43f2b5bdd3a3113362695593da427cc1a60805a7
Ricky Amparo <rickyamparo@gmail.com>
2017-12-30 15:07:56 -0700

    create dashboard view

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { View, TextInput, StyleSheet, TouchableOpacity, Text } from 'react-native';
 
 export default class Dashboard extends Component {
-  constructor() {
-
-  }
+  
   render (){
     return (
       <View>
