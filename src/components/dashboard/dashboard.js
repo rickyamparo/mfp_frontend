d3b53882f1ecda49f3db9ff0862f7c96047ea953
Ricky Amparo <rickyamparo@gmail.com>
2017-12-30 13:24:34 -0700

    create dashboard view and imported it into navigator in app.js

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, TextInput, StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+export default class Dashboard extends Component {
+  constructor() {
+
+  }
+  render (){
+    return (
+      <View>
+        <Text> Dashboard </Text>
+      </View>
+    )
+  }
+}
