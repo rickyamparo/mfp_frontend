09677d1e4821bcc5662010c172ebd9ab4b0918f3
Ricky Amparo <rickyamparo@gmail.com>
2017-12-30 20:15:28 -0700

    add basic flex rows to the dashboard marking out where each icon is going to go

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, TextInput, StyleSheet, TouchableOpacity, Text, AsyncStorage } from 'react-native';
+import { View, Image, TextInput, StyleSheet, TouchableOpacity, Text, AsyncStorage } from 'react-native';
 
 export default class Dashboard extends Component {
 
@@ -18,10 +18,38 @@ export default class Dashboard extends Component {
   render (){
     return (
 
-      <View>
-        <Text> Dashboard </Text>
-        <Text> {this.state.authToken} </Text>
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require('../../images/home-icon.png')}
+          />
+        </View>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        </View>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#16a085'
+  },
+  logoContainer: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  logo: {
+    width: 100,
+    height: 100
+  }
+})
