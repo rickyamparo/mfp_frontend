ffb6387bd55fad1e723077ba93d1a5dff52c4fbe
Ricky Amparo <rickyamparo@gmail.com>
2017-12-30 20:52:25 -0700

    use icons to make each navigation points for each of the pages from the dashboard

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Image, TextInput, StyleSheet, TouchableOpacity, Text, AsyncStorage } from 'react-native';
+import { View, Image, TextInput, StyleSheet, TouchableOpacity, Text, AsyncStorage, StatusBar } from 'react-native';
 
 export default class Dashboard extends Component {
 
@@ -19,19 +19,47 @@ export default class Dashboard extends Component {
     return (
 
       <View style={styles.container}>
-        <View style={styles.logoContainer}>
-          <Image
-            style={styles.logo}
-            source={require('../../images/home-icon.png')}
-          />
+      <StatusBar barStyle='light-content'/>
+      <Text style={styles.header}> A React Native App by Ricky Amparo</Text>
+
+        <View style={styles.dashboardRow}>
+
+          <View style={styles.dashboardIcon}>
+            <Image
+              style={styles.logo}
+              source={require('../../images/location-icon.png')}
+            />
+            <Text style={styles.info}> Location Tracker </Text>
+          </View>
+
+          <View style={styles.dashboardIcon}>
+            <Image
+              style={styles.logo}
+              source={require('../../images/read-mind.png')}
+            />
+            <Text style={styles.info}> Predict Location </Text>
+          </View>
+
         </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+
+        <View style={styles.dashboardRow}>
+
+          <View style={styles.dashboardIcon}>
+            <Image
+              style={styles.logo}
+              source={require('../../images/user-icon2.png')}
+            />
+            <Text style={styles.info}> User Account </Text>
+          </View>
+
+          <View style={styles.dashboardIcon}>
+            <Image
+              style={styles.logo}
+              source={require('../../images/info-icon.png')}
+            />
+            <Text style={styles.info}> About MFP </Text>
+          </View>
+
         </View>
       </View>
     )
@@ -45,11 +73,35 @@ const styles = StyleSheet.create({
   },
   logoContainer: {
     alignItems: 'center',
-    flexGrow: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginTop: 30
   },
   logo: {
-    width: 100,
-    height: 100
+    width: 120,
+    height: 120
+  },
+  dashboardRow: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 60,
+    paddingVertical: 60
+  },
+  dashboardIcon: {
+    width: 125,
+    height: 125,
+  },
+  header: {
+    color: '#FFF',
+    marginTop: 5,
+    textAlign: 'center',
+    opacity: 0.7
+  },
+  info: {
+    color: '#FFF',
+    marginTop: 10,
+    textAlign: 'center',
+    opacity: 0.7,
+    fontSize: 15
   }
 })
